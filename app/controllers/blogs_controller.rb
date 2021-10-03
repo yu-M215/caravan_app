6dceb8e5d9c3e984acd4d8869aa9adeb0cf8fdb3
yu-M215 <yuu0609km.215@gmail.com>
2021-10-03 08:51:59 +0000

    [Fix] 記述ミスの修正

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,6 +1,8 @@
 class BlogsController < ApplicationController
   def index
     @blogs = Blog.all
+    p "標準出力にのみ反映"
+    logger.debug("標準出力とログファイルに記録される")
   end
 
   def show
@@ -14,7 +16,7 @@ class BlogsController < ApplicationController
   def create
     @blog = Blog.new(blog_params)
     if @blog.save
-      redirect_to blog_params(@blog.id)
+      redirect_to blog_path(@blog.id)
     else
       render :new
     end
@@ -39,7 +41,6 @@ class BlogsController < ApplicationController
   private
   def blog_params
     params.require(:blog).permit(:title,:category,:body)
-
   end
 
 end
